e6b570e7dd8af32722209ab74965f2176cb4bbc7
Alini Ribeiro <aliniribeiroo@gmail.com>
2024-10-25 13:05:35 -0300

    first example using linear regression to predict

---
+++ scripts/environment_config.sh
@@ -0,0 +1,10 @@
+# Install Jupyter
+#pip3 install ipykernel==6.29.5
+
+
+# Create a environment as Jupyter Kernel
+
+python -m ipykernel install --name ml_lini_kernel --display-name "ML lini Kernel" --prefix /Users/alini/Documents/lini-repo/jupyter_kernels/
+
+# After that, we should add the kernel into pycharm at file > settings> preferences > Python interpreter
+# If you didnt found the interpreter, go to the kernel json file and identify the address of the kernel
